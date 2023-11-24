330df7f897fe3702f06b23dcc67d465276998ac8
Hannah Nelson <nexec64@gmail.com>
2023-11-24 14:24:25 -0800

    minor tweaks to everything

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import cmd
 
-class CommandHandler(cmd.Cmd):
+class ChatClient(cmd.Cmd):
     def do_JOIN(self,arg):
+        #TODO
         print('join'+arg)
     def do_LIST(self,arg):
+        #TODO
         print('LIST'+arg)
     def do_MESG(self,arg):
+        #TODO
         print('MESG'+arg)
     def do_BCST(self,arg):
+        #TODO
         print('BCST'+arg)
     def do_QUIT(self,arg):
-        print('QUIT'+arg)
+        #TODO
+        """
+        disconnect and quit the program
+        """
+        self.disconnect()
+        quit()
+    def disconnect(self):
+        #TODO
+        """
+        disconnect from stuff
+        """
+        pass
 
 if __name__ == '__main__':
-    CommandHandler().cmdloop()
+    ChatClient().cmdloop()
