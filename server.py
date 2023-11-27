d15ae731ab8b6d757f92ecb45fb0ebb4c5e60537
Hannah Nelson <nexec64@gmail.com>
2023-11-26 22:48:06 -0800

    Garret made MESG work

--- server.py
+++ server.py
@@ -23,31 +23,23 @@ def create_server(port):  # Create server and bind port to IP; start listening
 
 
 def handle_user(user_socket, address):
+    sender = None
+
     while True:
-        try:
-            incoming = user_socket.recv(1024).decode('utf-8').strip()
-        except socket.error as e:
-            # Handle unexpected disconnection
-            print(f"User at {address} disconnected unexpectedly.")
-            break
-
-        if not incoming:
-            # Handle case where incoming is an empty string (client disconnected)
-            print(f"User at {address} disconnected.")
-            break
-
+        incoming = user_socket.recv(1024).decode('utf-8').strip()
         command, *args = incoming.split()
 
         if command == "JOIN":
             JOIN(name=args[0], user_socket=user_socket, address=address)
+            sender = args[0]  # Update sender when JOIN command is processed
         elif command == "LIST":
             LIST(user_socket)
         elif command == "MESG":
-            MESG(sender=args[0], recipientName=args[1], message=args[2])
+            MESG(sender, *args)
         elif command == "BCST":
             BCST(sender=args[0], message=args[1])
         elif command == "QUIT":
-            QUIT(user_socket=user_socket)
+            QUIT(name=args[0], user_socket=user_socket)
         else:
             user_socket.send("Unknown Message".encode("utf-8"))
 
@@ -73,24 +65,12 @@ def BCST(sender, message):
             user_socket.send(f'{str(name)} is sending a broadcast\n {message}'.encode('utf-8'))
 
 
-def QUIT(user_socket):
-    # Find the name associated with the given user_socket
-    name = find_name_by_socket(users, user_socket)
-
-    if name is not None:
-        print(f"{name} has left the chat.")
-        BCST(user_socket, f'{name} is quitting the chat server'.encode('utf-8'))
+def QUIT(name, user_socket):
+        print("f{name} has left the chat.")
+        #user_socket.send(f"You have left!".encode('utf-8'))
+        BCST(user_socket, '{name} is quitting the chat server')
         del users[name]
         user_socket.close()
-    else:
-        print("User not found for QUIT command.")
-
-    try:
-        # Check if the socket is still valid before sending a message
-        if user_socket.fileno() != -1:
-            user_socket.send("You are not registered or already left.".encode('utf-8'))
-    except OSError as e:
-        print(f"Error: {e}")
 
 
 def LIST(user_socket):
@@ -100,18 +80,26 @@ def LIST(user_socket):
     print(user_list)
 
 
-def MESG(sender, recipientName, message):
-    if recipientName in users:
-        rec_socket = users[recipientName]
-        rec_socket.send(f'Message from {sender} : {message}'.encode('utf-8'))
-    else:
-        users[sender].send(f'This name is not in the chat room'.encode('utf-8'))
+def MESG(sender, *args):
+    try:
+        if len(args) < 2:
+            raise ValueError("MESG command requires at least two arguments: recipientName and message")
 
-def find_name_by_socket(users, target_socket):
-    for name, socket_obj in users.items():
-        if socket_obj == target_socket:
-            return name
-    return None  # Return None if the socket object is not found
+        recipientName, message = args
+
+        if recipientName in users:
+            rec_socket = users[recipientName]
+            rec_socket.send(f'Message from {sender}: {message}'.encode('utf-8'))
+        else:
+            users[sender].send(f'This recipient name is not in the chat room'.encode('utf-8'))
+
+    except ValueError as ve:
+        users[sender].send(f'Error: {ve}'.encode('utf-8'))
+
+    except Exception as e:
+        print(f"Error processing MESG command: {e}")
+        users[sender].send(f"An error occurred while processing MESG command".encode('utf-8'))
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 server.py <svr_port>")
