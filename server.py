bd1e0d0ea15fcb11e8dc338002b638439cecf93d
Hannah Nelson <nexec64@gmail.com>
2023-11-26 20:45:19 -0800

    Nick fixed the QUIT

--- server.py
+++ server.py
@@ -24,7 +24,18 @@ def create_server(port):  # Create server and bind port to IP; start listening
 
 def handle_user(user_socket, address):
     while True:
-        incoming = user_socket.recv(1024).decode('utf-8').strip()
+        try:
+            incoming = user_socket.recv(1024).decode('utf-8').strip()
+        except socket.error as e:
+            # Handle unexpected disconnection
+            print(f"User at {address} disconnected unexpectedly.")
+            break
+
+        if not incoming:
+            # Handle case where incoming is an empty string (client disconnected)
+            print(f"User at {address} disconnected.")
+            break
+
         command, *args = incoming.split()
 
         if command == "JOIN":
@@ -36,7 +47,7 @@ def handle_user(user_socket, address):
         elif command == "BCST":
             BCST(sender=args[0], message=args[1])
         elif command == "QUIT":
-            QUIT(name=args[0], user_socket=user_socket)
+            QUIT(user_socket=user_socket)
         else:
             user_socket.send("Unknown Message".encode("utf-8"))
 
@@ -62,12 +73,24 @@ def BCST(sender, message):
             user_socket.send(f'{str(name)} is sending a broadcast\n {message}'.encode('utf-8'))
 
 
-def QUIT(name, user_socket):
-        print("f{name} has left the chat.")
-        #user_socket.send(f"You have left!".encode('utf-8'))
-        BCST(user_socket, '{name} is quitting the chat server')
+def QUIT(user_socket):
+    # Find the name associated with the given user_socket
+    name = find_name_by_socket(users, user_socket)
+
+    if name is not None:
+        print(f"{name} has left the chat.")
+        BCST(user_socket, f'{name} is quitting the chat server'.encode('utf-8'))
         del users[name]
         user_socket.close()
+    else:
+        print("User not found for QUIT command.")
+
+    try:
+        # Check if the socket is still valid before sending a message
+        if user_socket.fileno() != -1:
+            user_socket.send("You are not registered or already left.".encode('utf-8'))
+    except OSError as e:
+        print(f"Error: {e}")
 
 
 def LIST(user_socket):
@@ -84,7 +107,11 @@ def MESG(sender, recipientName, message):
     else:
         users[sender].send(f'This name is not in the chat room'.encode('utf-8'))
 
-
+def find_name_by_socket(users, target_socket):
+    for name, socket_obj in users.items():
+        if socket_obj == target_socket:
+            return name
+    return None  # Return None if the socket object is not found
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 server.py <svr_port>")
