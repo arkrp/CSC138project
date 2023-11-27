0f2eae122fae161c48ce301ba65148cf7b1eefe9
Hannah Nelson <nexec64@gmail.com>
2023-11-26 18:30:47 -0800

    added Fabby's client changes

--- client.py
+++ client.py
@@ -1,53 +1,58 @@
-import cmd, sys, socket
+import cmd
+import sys
+import socket
 
 def main():
     verifyarguments()
     print(sys.argv)
-    connection = makeconnection('127.0.0.1',int(sys.argv[1]));
+    server_ip = sys.argv[1]
+    server_port = int(sys.argv[2])
+    connection = makeconnection(server_ip, server_port)
     ChatClient(connection).cmdloop()
 
 def verifyarguments():
-    if(len(sys.argv) == 1):
+    if len(sys.argv) == 1:
         print('incorrect number of arguments supplied\nusage: python3 client.py <server_port>')
         exit(1)
 
-def makeconnection(serverName,serverPort):
-    TCPSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def makeconnection(serverName, serverPort):
+    TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     TCPSocket.settimeout(3)
     try:
-        TCPSocket.connect((serverName,serverPort))
+        TCPSocket.connect((serverName, serverPort))
         return TCPSocket
     except (socket.timeout, ConnectionRefusedError):
         print('No response to connection. Did you choose the right port?')
         quit(1)
-    
 
 class ChatClient(cmd.Cmd):
-    #TODO add some sort of passive listener for incoming messages? that sounds kind of hard to be honest
+    # TODO add some sort of passive listener for incoming messages? that sounds kind of hard to be honest
     __slots__ = 'connection'
-    def __init__(self,connection):
+
+    def __init__(self, connection):
         super().__init__()
         self.connection = connection
-    def do_JOIN(self,arg):
-        self.connection.sendall(b'JOIN')
-        #TODO actual logic for this
-    def do_LIST(self,arg):
-        self.connection.sendall(b'LIST')
-        print('LIST'+arg)
-        #TODO actual logic for this
-    def do_MESG(self,arg):
-        self.connection.sendall(b'MESG')
-        print('MESG'+arg)
-        #TODO actual logic for this
-    def do_BCST(self,arg):
-        self.connection.sendall(b'BCST')
-        print('BCST'+arg)
-        #TODO actual logic for this
-    def do_QUIT(self,arg):
+
+    def send_message(self, message):
+        self.connection.sendall((message + '\n').encode('utf-8'))
+
+    def do_JOIN(self, arg):
+        self.send_message(f'JOIN {arg}')
+
+    def do_LIST(self, arg):
+        self.send_message('LIST')
+
+    def do_MESG(self, arg):
+        self.send_message(f'MESG {arg}')
+
+    def do_BCST(self, arg):
+        self.send_message(f'BCST {arg}')
+
+    def do_QUIT(self, arg):
         """
         Disconnect from the server and quit the program
         """
-        self.connection.sendall(b'QUIT')
+        self.send_message('QUIT')
         self.connection.close()
         quit(0)
 
